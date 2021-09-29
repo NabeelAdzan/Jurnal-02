21a7ca1e10510adfa51dddff4f94fe796bf26fe4
Nabeel Adzan <muhnabeeladzan@gmail.com>
2021-09-29 09:33:21 +0700

    First Commit.

---
+++ HelloNabeel.java
@@ -0,0 +1,7 @@
+public class HelloNabeel {
+    public static void main(String[] args) {
+        System.out.println("Nama  : Nabeel");
+        System.out.println("NIM   : D3IF6706213044");
+        System.out.println("Kelas : 45-01");
+    }
+}
---
+++ contoh.java
@@ -0,0 +1,14 @@
+
+public class contoh {
+    public static void main(String[] args) {
+        //Print string secara langsung tanpa variabel
+        System.out.println("putih");
+        //string selalu dikurung dalam "string" atau 'string'
+
+        //Print string melalui variabel
+        String teks = "hitam";
+
+        System.out.println(teks);
+
+    }
+}
